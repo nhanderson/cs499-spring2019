fe15cc5f8b0df9a109b0b445d89c943e19140d83
Megan Mikami <mmm924@nau.edu>
2019-02-01 22:23:31 -0700

    comments for clarifying matricies and vectors, manhattan distance formula added

--- project-1-nearest-neighbors/nearestNeighbors/src/nn.cpp
+++ project-1-nearest-neighbors/nearestNeighbors/src/nn.cpp
@@ -1,5 +1,6 @@
 #include "nn.h"
 #include <Eigen>
+#include <stdlib.h> // for abs
 
 int NN1toKmaxPredict( int n_train_observations, int n_test_observations, int n_features, 
                       int max_neighbors, double *train_in_ptr, double *train_out_ptr, 
@@ -9,11 +10,16 @@ int NN1toKmaxPredict( int n_train_observations, int n_test_observations, int n_f
   
   Eigen::VectorXd distance_vec(n_train_observations);
   // Use Eigen to map 
-  Eigen::Map< Eigen::MatrixXd > train_in_mat(train_in_ptr, n_train_observations, n_features);
-  Eigen::Map< Eigen::VectorXd > test_in_vec(test_in_ptr, n_features);
-  Eigen::VectorXd diff_vec(ncol);
+  Eigen::Map< Eigen::MatrixXd > train_in_mat(train_in_ptr, n_train_observations, n_features); // matrix training data inputs
+  Eigen::Map< Eigen::VectorXd > train_out_vec(train_out_ptr, n_train_observations); // vector training data outputs
+  Eigen::Map< Eigen::MatrixXd > test_in_mat(test_in_ptr, n_test_observations, n_features); // matrix test data input
+  Eigen::Map< Eigen::VectorXd > test_in_vec(test_in_ptr, n_features); // not sure what this is for
+  Eigen::VectorXd diff_vec(n_features);
+  Eigen::VectorXi sorted_index_vec(n_train_observations)
   for(int i=0; i<n_train_observations; i++){
-    diff_vec = train_input_mat.row(i).transpose()-test_in_ptr;
-    distance_vec(i) = diff_vec
+    diff_vec = abs(train_in_mat.row(i).transpose()-test_in_vec) + abs(test_in_mat.row(i).transpose()-train_out_vec); // |test x-train x| + |test y-train y|
+    distance_vec(i) = diff_vec.norm(); 
+    sorted_index_vec(i) = i;
   }
+  
 }
