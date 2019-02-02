637d1c6e3ca5a05d3fe87fc77828000abb776444
Jacob Lemon <jacoblemon@Jacobs-MacBook-Air-2.local>
2019-02-01 18:23:29 -0700

    added distance for loop

--- project-1-nearest-neighbors/nearestNeighbors/src/nn.cpp
+++ project-1-nearest-neighbors/nearestNeighbors/src/nn.cpp
@@ -7,8 +7,13 @@ int NN1toKmaxPredict( int n_train_observations, int n_test_observations, int n_f
 {
   // Error Checking
   
-  // Use Eigen to map  
+  Eigen::VectorXd distance_vec(n_train_observations);
+  // Use Eigen to map 
   Eigen::Map< Eigen::MatrixXd > train_in_mat(train_in_ptr, n_train_observations, n_features);
   Eigen::Map< Eigen::VectorXd > test_in_vec(test_in_ptr, n_features);
-  //
+  Eigen::VectorXd diff_vec(ncol);
+  for(int i=0; i<n_train_observations; i++){
+    diff_vec = train_input_mat.row(i).transpose()-test_in_ptr;
+    distance_vec(i) = diff_vec
+  }
 }
