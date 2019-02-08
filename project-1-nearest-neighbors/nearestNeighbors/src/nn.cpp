9c1a6b275b6f2c1bd37122793deb1407354df9d9
Nick Anderson <nha4@nau.com>
2019-02-07 21:05:05 -0700

    Corrected some syntax errors

--- project-1-nearest-neighbors/nearestNeighbors/src/nn.cpp
+++ project-1-nearest-neighbors/nearestNeighbors/src/nn.cpp
@@ -1,5 +1,5 @@
 #include "nn.h"
-#include <Eigen>
+#include <Eigen/Dense>
 #include <stdlib.h> // for abs
 
 int NN1toKmaxPredict( int n_train_observations, int n_test_observations, int n_features, 
@@ -15,20 +15,20 @@ int NN1toKmaxPredict( int n_train_observations, int n_test_observations, int n_f
   Eigen::Map< Eigen::MatrixXd > test_in_mat(test_in_ptr, n_test_observations, n_features); // matrix test data input
   Eigen::Map< Eigen::VectorXd > test_in_vec(test_in_ptr, n_features); // not sure what this is for
   Eigen::VectorXd diff_vec(n_features);
-  Eigen::VectorXi sorted_index_vec(n_train_observations)
+  Eigen::VectorXi sorted_index_vec(n_train_observations);
   for(int i=0; i<n_train_observations; i++){
-    diff_vec = abs(train_in_mat.row(i).transpose()-test_in_vec) + abs(test_in_mat.row(i).transpose()-train_out_vec); // |test x-train x| + |test y-train y|
+    diff_vec = abs(train_in_mat.row(i).transpose()-test_in_vec) + abs(test_in_mat.row(i).transpose()- train_out_vec); // |test x-train x| + |test y-train y|
     distance_vec(i) = diff_vec.norm(); 
     sorted_index_vec(i) = i;
   }
-  std::sort(sorted_index_vec.data(), sored_index_vec.data() + sorted_index_vec.size(), 
-            [&distance_vec](int left, int right){ return distance_vec(left) < distance_vec(right)});
+  std::sort(sorted_index_vec.data(), sorted_index_vec.data() + sorted_index_vec.size(), 
+            [&distance_vec](int left, int right){ return distance_vec(left) < distance_vec(right);});
   double total_observations = 0.0;
   for( int k = 0; k < max_neighbors; k++ ){
     int row = sorted_index_vec(k);
     int neighbors = k+1;
     total_observations += train_out_ptr[row];
-    preditions_out_ptr[k] = total_observations/neighbors; 
+    predictions_out_ptr[k] = total_observations/neighbors; 
   }
   return 0;
 }
