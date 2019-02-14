a36e3beff9e9a277ffe1d49c2310d94207fd051e
Megan Mikami <mmm924@nau.edu>
2019-02-13 21:11:26 -0700

    after screencast 2 and 3

--- project-1-nearest-neighbors/nearestNeighbors/src/nn.cpp
+++ project-1-nearest-neighbors/nearestNeighbors/src/nn.cpp
@@ -2,9 +2,14 @@
 #include <Eigen/Dense>
 #include <stdlib.h> // for abs
 
-int NN1toKmaxPredict( int n_train_observations, int n_test_observations, int n_features, 
-                      int max_neighbors, double *train_in_ptr, double *train_out_ptr, 
-                      double *test_in_ptr, double *predictions_out_ptr)
+int NN1toKmaxPredict( const int n_train_observations, 
+                      const int n_test_observations, 
+                      const int n_features, 
+                      const int max_neighbors, 
+                      const double *train_in_ptr,   //n_observations x n_features 
+                      const double *train_out_ptr,  //n_observations
+                      const double *test_in_ptr,    //n_features
+                      double *predictions_out_ptr ) //max_neighbors
 {
   // Error Checking
   if( max_neighbors < 0 ){
@@ -19,24 +24,25 @@ int NN1toKmaxPredict( int n_train_observations, int n_test_observations, int n_f
   if( n_features < 0 ){
     return INVALID_NUM_FEATURES;
   }
-  
-  Eigen::VectorXd distance_vec(n_train_observations);
+ 
   // Use Eigen to map 
-  Eigen::Map< Eigen::MatrixXd > train_in_mat(train_in_ptr, n_train_observations, n_features); // matrix training data inputs
-  Eigen::Map< Eigen::VectorXd > train_out_vec(train_out_ptr, n_train_observations); // vector training data outputs
-  Eigen::Map< Eigen::MatrixXd > test_in_mat(test_in_ptr, n_test_observations, n_features); // matrix test data input
-  Eigen::Map< Eigen::VectorXd > test_in_vec(test_in_ptr, n_features); // not sure what this is for
-  Eigen::VectorXd diff_vec(n_features);
+  Eigen::Map< Eigen::MatrixXd > train_in_mat((double*) train_in_ptr, n_train_observations, n_features); // matrix training data inputs
+  Eigen::Map< Eigen::VectorXd > test_in_vec((double*) test_in_ptr, n_features); // vector test data inputs
+  Eigen::Map< Eigen::MatrixXd > test_in_mat((double*)test_in_ptr, n_test_observations, n_features); // matrix test data input
+  Eigen::Map< Eigen::VectorXd > train_out_vec((double*)train_out_ptr, n_train_observations); // vector training data outputs
+  Eigen::VectorXd dist_vec(n_features);
   Eigen::VectorXi sorted_index_vec(n_train_observations);
-    
   
   for(int i=0; i<n_train_observations; i++){
-    diff_vec = abs(train_in_mat.row(i).transpose().array()-test_in_vec.array()) + abs(test_in_mat.row(i).transpose().array()-train_out_vec.array()); // |test x-train x| + |test y-train y|
-    distance_vec(i) = diff_vec.norm(); 
+    dist_vec = abs(train_in_mat.row(i).transpose().array()-test_in_vec.array()) + abs(test_in_mat.row(i).transpose().array()-train_out_vec.array()); // |test x-train x| + |test y-train y|
+    dist_vec(i) = dist_vec.norm(); 
     sorted_index_vec(i) = i;
   }
-  std::sort(sorted_index_vec.data(), sorted_index_vec.data() + sorted_index_vec.size(), 
-            [&distance_vec](int left, int right){ return distance_vec(left) < distance_vec(right);});
+  
+  std::sort(sorted_index_vec.data(), 
+            sorted_index_vec.data() + sorted_index_vec.size(), 
+            [&dist_vec](int left, int right){ return dist_vec(left) < dist_vec(right);});
+  
   double total_observations = 0.0;
   for( int k = 0; k < max_neighbors; k++ ){
     int row = sorted_index_vec(k);
@@ -44,5 +50,8 @@ int NN1toKmaxPredict( int n_train_observations, int n_test_observations, int n_f
     total_observations += train_out_ptr[row];
     predictions_out_ptr[k] = total_observations/neighbors; 
   }
-  return NO_ERR;
+  
+  // Eigen::VectorXd distance_vec(n_train_observations);
+
+  return 0;
 }
