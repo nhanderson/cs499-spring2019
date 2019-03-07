816c0839ef2245fe70bc41a92851795e2df121a5
Megan Mikami <mmm924@nau.edu>
2019-03-06 22:30:32 -0700

    finished LMSquareLossIteration function, need to finish example in documentation

--- project-2-linear-models/linearModels/R/EarlyStopping.R
+++ project-2-linear-models/linearModels/R/EarlyStopping.R
@@ -1,16 +1,26 @@
-#' scaling input matix
-Scaling <- function(X.mat){
-  if( !is.matrix(X.mat)){
-    stop("X.mat must be a matrix")
-  }
-  .colMeans(X.mat, m, n, na.rm = TRUE);
-  scale( X.mat, center = m,scale = TURE );
-  
-}
-
-
-
-
+#' LMSquareLossIterations function
+#' 
+#' R function that wraps the C++ code.
+#'
+#' @param X.mat numeric train featrue matrix [n x p]
+#' @param y.vec numeric train label vector [n]
+#' @param max.iteratons scalar integer greater than 1, max number of iterations
+#' @param step.size scalar integer, step size
+#'
+#' @return matrix of weight vectors 
+#' @export 
+#'
+#' @examples
+#' data(zip.train, package="ElemStatLearn")
+#' i01 <- which(zip.train[,1] %in% c(0,1))
+#' train.i <- i01[1:5]
+#' test.i <- i01[6]
+#' x <- zip.train[train.i, -1]
+#' y <- zip.train[train.i, 1]
+#' testx <- zip.train[test.i, -1]
+#' NN1toKmaxPredict(x ,y, testx , as.integer(3))
+#' zip.train[test.i, 1]
+#' 
 
 LMSquareLossIterations <- function(X.mat, y.vec, max.iterations, step.size){
   if(!is.matrix(X.mat)){
@@ -28,17 +38,21 @@ LMSquareLossIterations <- function(X.mat, y.vec, max.iterations, step.size){
   if(!is.integer(step.size)){
     stop("step.size must be an integer")
   }
+  
+   <- scale( X.mat, center = FALSE, scale = apply( X.mat, 2, na.rm = TRUE) )
+  
   result<- .C("LMSquareLossIterations_interface", 
               as.integer(nrow(X.mat)),  #' n_train
               as.integer(nrow(X.mat)),  #' n_test
               as.integer(ncol(X.mat)),  #' n_features
               as.integer(max.interations),  #' max_iterations
               as.integer(step.size),   #' step_size
-              as.double(X.mat),  #' train_input_ptr
+              as.double(scaledX.mat),  #' train_input_ptr
               as.double(y.vec),  #' train_output_ptr
               weight_mat = double(ncol(X.mat) * max.iterations)  #' predictions_output_ptr
               )
-  matrix(result.list$weight_mat, ncol(X.mat), max_iterations)
+  weight.mat <- matrix(result.list$weight_mat, ncol(X.mat), max_iterations)
+  unscaled.weight.mat <- unscale( weight.mat, X.mat )
 }
 
 
