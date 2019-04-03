d8748bc7995411c73e66314ae8523bf86f10032b
Nick Anderson <nicksspleen@gmail.com>
2019-04-03 03:53:07 -0700

    fixed indents

--- project-3-neural-networks/neuralNetworks/R/neuralNetworks.R
+++ project-3-neural-networks/neuralNetworks/R/neuralNetworks.R
@@ -112,3 +112,105 @@ NNetIterations <- function( X.mat, y.vec, max.iterations, step.size, n.hidden.un
   
   
 }
+
+#' NNetEarlyStoppingCV
+#' 
+#' R function that calculates the precition function of a neural network using n-hidden units
+#' 
+#' @param X.mat feature matrix (n_observations x n_features)
+#' @param y.vec label vector (n_observations x 1)
+#' @param fold.vec fold ID vector (n_observations x 1)
+#' @param max.iterations int scalar > 1
+#' @param step.size
+#' @param n.hidden.units number of hidden units
+#' 
+#' @return list with pred.mat, V.mat, x.vec, predict(testX.mat), mean.validation.loss, mean.train.loss.vec, selected.steps
+#'
+#' @examples
+#' 
+NNetEarlyStoppingCV <- function( X.mat, y.vec, fold.vec, max.iterations, step.size, n.hidden.units ){
+  if(!is.matrix(X.mat)){
+    stop("X.mat must be a matrix")
+  }
+  if(!is.vector(y.vec)){
+    stop("y.vec must be a vector")
+  }
+  if(!(length(y.vec)==nrow(X.mat))){
+    stop("y.vec must have the same length of X.mat rows")
+  }
+  if(!is.vector(fold.vec)){
+    stop("fold.vec must be a vector")
+  }
+  if(!(length(fold.vec)==nrow(X.mat))){
+    stop("fold.vec must have the same length of X.mat rows")
+  }
+  if(!is.integer(max.iterations)){
+    stop("max.neighbors must be an integer")
+  }
+  if(!((is.integer(max.iterations) && max.iterations > 1))){
+    stop("max.iterations must be an integer greater than 1")
+  }
+  if(!is.numeric(step.size)){
+    stop("step.size must be a numeric value")
+  }
+  if(!(is.numeric(step.size) && step.size > 0)){
+    stop("step.size must be a numeric value greater than 0")
+  }
+  if(!(is.integer(n.hidden.units) && n.hidden.units > 0)){
+    stop("n.hidden.units must be an integer greater than 0")
+  }
+  
+  # should use K-fold cross-validation based on the fold IDs provided in fold.vec
+  fold.ids <- unique(fold.vec)
+  
+  # initalize loss matrix to store loss values for each fold
+  fold.validation.loss.mat <- matrix(0, length(fold.ids), max.iterations)
+  fold.train.loss.mat <- matrix(0, length(fold.ids), max.iterations)
+  
+  for(fold.i in fold.ids){
+    is.validation <- which(fold.vec == fold.i)
+    is.train <- which(fold.vec != fold.i)
+    
+    # TODO for each train/validation split, use NNetIterations to compute the predictions for all observations
+    fold.result <- NNetIterations( X.mat, y.vec, max.iterations, step.size, n.hidden.units, is.train )
+    fold.pred.mat <- fold.result$pred.mat
+    
+    # TODO calculate the loss for the fold 
+    # use the square loss for regression and the 01-loss for binary classification
+    fold.validation.loss <-
+    fold.train.loss <-
+      
+    # store fold loss in loss matrix
+    fold.validation.loss.mat[fold.i, ] <- fold.validation.loss
+    fold.train.loss.mat[fold.i, ] <- fold.train.loss
+  }
+  # TODO compute mean.validation.loss.vec, which is a vector (with max.iterations elements) of mean validation loss over all K
+  # folds (use the square loss for regression and the 01-loss for binary classification).
+  mean.validation.loss.vec <- 
+    
+  # TODO compute mean.train.loss.vec, analogous to above but for the train data.
+  mean.train.loss.vec <- 
+    
+  # minimize the mean validation loss to determine selected.steps, the optimal number of steps/iterations.
+  selected.steps <- which.min(mean.validation.loss.vec)
+  
+  # TODO finally use NNetIterations(max.iterations=selected.steps) on the whole training data set.
+  is.train <- rep(TRUE, nrow(X.mat))
+  final.result <- NNetIterations( X.mat, y.vec, selected.steps, step.size, n.hidden.units, is.train )
+  
+  # Output the same list from NNetIterations
+  # pred.mat, n_observations x max.iterations matrix of predicted values
+  # V.mat final weight matrix (n_features+1 x n.hidden.units). The first row of V.mat should be the intercept terms.
+  # w.vec final weight vector (n.hidden.units+1). The first element of w.vec should be the intercept term.
+  # predict(testX.mat), a function that takes an unscaled test feature matrix and returns a vector of predictions
+  # mean.validation.loss, mean.train.loss.vec (for plotting train/validation loss curves)
+  # selected.steps
+  
+  return (list(pred.mat=final.result$pred.mat, 
+               V.mat=final.result$V.mat, 
+               w.vec=final.result$w.vec, 
+               predict(testX.mat)=final.result$predict(testX.mat), 
+               mean.validation.loss=mean.validation.loss, 
+               mean.train.loss.vec=mean.train.loss.vec, 
+               selected.steps=selected.steps))
+}
