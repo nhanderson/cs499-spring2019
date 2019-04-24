dd8ee4039573e981ffd43b9c1523361e888eb845
Nick Anderson <nicksspleen@gmail.com>
2019-04-23 23:06:24 -0700

    Updated function params, LinearModelL1CV

--- project-4-L1-regularization/l1regularization/R/l1regularization.R
+++ project-4-L1-regularization/l1regularization/R/l1regularization.R
@@ -2,14 +2,14 @@
 #' 
 #' 
 #' 
-#' @param X.scaled.mat
-#' @param y.vec
-#' @param penalty (non-negative numeric scalar)
-#' @param opt.thresh (positive numeric scalar)
-#' @param initial.weight.vec
-#' @param step.size
+#' @param X.scaled.mat scaled feature matrix (n_observations x n_features)
+#' @param y.vec label vector (n_observations x 1)
+#' @param penalty non-negative numeric scalar
+#' @param opt.thresh positive numeric scalar, threshold on the sub-differential optimality criterion
+#' @param initial.weight.vec weight vector (n_observations x 1)
+#' @param step.size int scalar > 1
 #' 
-#' @return optimal weight vector 
+#' @return w.opt optimal weight vector (n_observations+1 x 1)
 #'
 #' @examples
 #' 
@@ -41,7 +41,7 @@ LinearModelL1 <- function( X.scaled.mat, y.vec, penalty, opt.thresh, initial.wei
   }
   
   soft <- function(x,lambda){
-    sign(x) * (abs(x-lambda))
+    sign(x) * posPart(abs(x)-lambda))
   }
   
   sign <- function(x){
@@ -51,8 +51,7 @@ LinearModelL1 <- function( X.scaled.mat, y.vec, penalty, opt.thresh, initial.wei
   }
   
   posPart <- function(x) {
-    if( x > 0 ) { x } 
-    else { 0 }
+    ifelse( x > 0, x, 0 )
   }
   
   if(is.01){
@@ -62,8 +61,12 @@ LinearModelL1 <- function( X.scaled.mat, y.vec, penalty, opt.thresh, initial.wei
   grad.loss <- function(w.vec){
     X.int <- cbind(1, X.scaled.mat)
     pred.vec <- X.int %*% w.vec
-    prod.vec <- sigmoid(-pred.vec * y.tilde)
-    -t(X) %*% (y.tilde * prod.vec) 
+    if(is.01){
+      prod.vec <- sigmoid(-pred.vec * y.tilde)
+      -t(X) %*% (y.tilde * prod.vec)
+    } else {
+      
+    }
   }
   
   d.vec <- grad.loss(w.vec)
@@ -78,12 +81,12 @@ LinearModelL1 <- function( X.scaled.mat, y.vec, penalty, opt.thresh, initial.wei
 #' 
 #' 
 #' 
-#' @param X.mat
-#' @param y.vec
-#' @param penalty.vec
-#' @param step.size
+#' @param X.mat feature matrix (n_observations x n_features)
+#' @param y.vec label vector (n_observations x 1)
+#' @param penalty.vec non-negative numeric scalar
+#' @param step.size numeric scalar for step size 
 #' 
-#' @return 
+#' @return W.mat weight matrix on original scale (n_features+1 x n_penalties)
 #'
 #' @examples
 #' 
@@ -117,14 +120,14 @@ LinearModelL1penalties <- function( X.mat, y.vec, penalty.vec, step.size ){
 #' 
 #' 
 #' 
-#' @param X.mat
-#' @param y.vec
-#' @param fold.vec
-#' @param n.folds
+#' @param X.mat feature matrix (n_observations x n_features)
+#' @param y.vec label vector (n_observations x 1)
+#' @param fold.vec fold ID vector (n_observations x 1)
+#' @param n.folds int scalar > 1
 #' @param penalty.vec
-#' @param step.size
+#' @param step.size int scalar > 1
 #' 
-#' @return 
+#' @return mean.validation.loss, mean.train.loss.vec, penalty.vec, selected.penalty, weight.vec, predict=function(testX.mat)
 #'
 #' @examples
 LinearModelL1CV <- function( X.mat, y.vec, fold.vec, n.folds=5, penalty.vec, step.size ){
@@ -147,6 +150,58 @@ LinearModelL1CV <- function( X.mat, y.vec, fold.vec, n.folds=5, penalty.vec, ste
     stop("step.size must be numeric and greater than zero")
   }
   
-  set.seed(1)
-
+  # should use K-fold cross-validation based on the fold IDs provided in fold.vec
+  fold.ids <- unique(fold.vec)
+  
+  # determine if v.vec is binary to be used during loss calculation
+  is.binary <- all(y.vec %in% c(0,1))
+  
+  # initalize loss matrix to store loss values for each fold
+  fold.validation.loss.mat <- matrix(0, length(fold.ids), length(penalty.vec))
+  fold.train.loss.mat <- matrix(0, length(fold.ids), length(penalty.vec))
+  
+  for(fold.i in fold.ids){
+    is.validation <- which(fold.vec == fold.i)
+    is.train <- which(fold.vec != fold.i)
+    
+    # For each train/validation split, use LinearModelL1penalties to compute the predictions for all observations
+    fold.result <- LinearModelL1penalties( X.mat, y.vec, penalty.vec, step.size )
+    fold.pred <- cbind(1, X.mat) %*% fold.result$W.mat
+    
+    # Calculate the loss for the fold 
+    # use the square loss for regression and the 01-loss for binary classification
+    fold.validation.loss <- if(is.binary){
+      log(1+exp(-y.vec[is.validation]))
+    }else{
+      (fold.pred[is.validation]- y.vec[is.validation])^2 / nrow(X.mat[is.validation]) 
+    }
+    
+    fold.train.loss <- if(is.binary){
+      log(1+exp(-y.vec[is.train]))
+    }else{
+      (fold.pred[is.train] - y.vec[is.train])^2 / nrow(X.mat[is.train]) 
+    }
+    
+    # store fold loss in loss matrix
+    fold.validation.loss.mat[fold.i, ] <- fold.validation.loss
+    fold.train.loss.mat[fold.i, ] <- fold.train.loss
+  }
+  
+  mean.validation.loss.vec <- colMeans(fold.validation.loss.mat)
+  mean.train.loss.vec <- colMeans(fold.train.loss.mat)
+  
+  selected.penalty <- which.min(mean.validation.loss.vec)
+  
+  final.w.vec <- LinearModelL1penalties( X.mat, y.vec, selected.penalty, step.size )
+  
+  final.result$mean.validation.loss <- mean.validation.loss
+  final.result$mean.train.loss.vec <-mean.train.loss.vec
+  final.result$penalty.vec <- penalty.vec
+  final.result$selected.penalty <- selected.penalty
+  final.result$weight.vec <- final.w.vec
+  final.result$predict <- function(testX.mat){
+    
+  }
+  
+  return(final.result)
 }
