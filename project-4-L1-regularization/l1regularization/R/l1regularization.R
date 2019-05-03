c7888a72bd93e6ab1062d1d76daacfc340bb2795
Nick Anderson <nicksspleen@gmail.com>
2019-05-03 08:09:33 -0700

    Fixed errors in LinearModelL1, added example

--- project-4-L1-regularization/l1regularization/R/l1regularization.R
+++ project-4-L1-regularization/l1regularization/R/l1regularization.R
@@ -10,9 +10,30 @@
 #' @param step.size int scalar > 1
 #' 
 #' @return w.opt optimal weight vector (n_observations+1 x 1)
+#' 
+#' @export
 #'
 #' @examples
-#' 
+#' data(zip.train, package = "ElemStatLearn")
+#' all.y.vec <- zip.train[,1]
+#' is.01<- all.y.vec %in% c(0,1)
+#' y.vec <- all.y.vec[is.01]
+#' X.mat <- zip.train[is.01, -1]
+#' n.folds <- 5
+#' fold.vec <- sample(rep(1:n.folds, l=nrow(X.mat)))
+#' validation.fold <- 1
+#' is.train <- fold.vec != validation.fold
+#' is.validation <- fold.vec == validation.fold
+#' X.train <- X.mat[is.train,]
+#' y.train <- y.vec[is.train]
+#' X.scaled.mat <- scale(X.train)
+#' X.filtered <- X.scaled.mat[, attr(X.scaled.mat, "scaled:scale") != 0]
+#' opt.thresh <- 0.01
+#' initial.weight.vec <- rep( 0, l=ncol(X.filtered) +1)
+#' step.size <- 0.1
+#' penalty <- 1
+#' weight.vec <- LinearModelL1( X.filtered, y.train, penalty, opt.thresh, initial.weight.vec, step.size )
+
 LinearModelL1 <- function( X.scaled.mat, y.vec, penalty, opt.thresh, initial.weight.vec, step.size ){
   if(!is.matrix(X.scaled.mat)){
     stop("X.scaled.mat must be a matrix")
@@ -34,9 +55,8 @@ LinearModelL1 <- function( X.scaled.mat, y.vec, penalty, opt.thresh, initial.wei
   }
 
   #initialize w w/ intercept col, first term is bias/intercept
-  w <- rep( 0, l=ncol(X.scaled.mat)+1 )
-  
-  X.filtered <- X.scaled.mat[ , attr(X.scaled.mat, "scaled:scale") != 0]
+  X.int <- cbind(1, X.scaled.mat)
+  w.vec <- initial.weight.vec
   
   sigmoid <- function(z){
     1/(1+exp(-z))
@@ -64,36 +84,42 @@ LinearModelL1 <- function( X.scaled.mat, y.vec, penalty, opt.thresh, initial.wei
   
   is.opt <- function(d.vec, w.vec) {
     result.vec <- rep( 0, length(w.vec) )
-    for(index in length(w.vec)){
+    for(index in 1:length(w.vec)){
       if(w.vec[index] != 0) {
         result.vec[index] <- abs(d.vec[index] - (sign(w.vec[index])))
       } else {
-        result.vec[index] <- abs(d.vec)
+        result.vec[index] <- abs(d.vec[index])
       }
-      ifelse(length(result.vec[result.vec < opt.thresh]), TRUE, FALSE)
     }
+    ifelse(length(result.vec[result.vec < opt.thresh]) == length(result.vec), TRUE, FALSE)
   }
   
-  while(!(is.opt)){
-    grad.loss <- function(w.vec){
-      X.int <- cbind(1, X.scaled.mat)
-      pred.vec <- X.int %*% w.vec
-      if(is.01){
-        prod.vec <- sigmoid(-pred.vec * y.tilde)
-        -t(X) %*% (y.tilde * prod.vec)
-      } else {
-        pred.vec - y.vec
-      }
-      
-      #1/n ∑i=1^n L[w^T x_i + b, y_i] + penalty * ||w||_1
+  grad.loss <- function(X.int, w.vec){
+    
+    pred.vec <- X.int %*% w.vec
+    if(is.01){
+      prod.vec <- sigmoid(-pred.vec * y.tilde)
+      -t(X.int) %*% (y.tilde * prod.vec)
+    } else {
+      pred.vec - y.vec
     }
     
-    d.vec <- grad.loss(w.vec)
+    #1/n ∑i=1^n L[w^T x_i + b, y_i] + penalty * ||w||_1
+  }
+  
+  d.vec <- grad.loss(X.int, w.vec)
+  
+  u.vec <- w.vec + step.size * d.vec
+  
+  while(is.opt(d.vec, w.vec) == FALSE){
+    
+    d.vec <- grad.loss(X.int, w.vec)
     
     u.vec <- w.vec + step.size * d.vec
     
     w.vec <- c(u.vec[1],soft(u.vec[-1], step.size * opt.thresh))
   }
+  
   weight.vec <- w.vec
   return (weight.vec)
   
@@ -109,6 +135,8 @@ LinearModelL1 <- function( X.scaled.mat, y.vec, penalty, opt.thresh, initial.wei
 #' @param step.size numeric scalar for step size 
 #' 
 #' @return W.mat weight matrix on original scale (n_features+1 x n_penalties)
+#' 
+#' @export
 #'
 #' @examples
 #' 
@@ -129,26 +157,29 @@ LinearModelL1penalties <- function( X.mat, y.vec, penalty.vec, step.size ){
   # scale X.mat
   X.scaled.mat <- scale(X.mat)
   
+  # filter X.scaled.mat to remove 0 values
+  X.filtered <- X.scaled.mat[ , attr(X.scaled.mat, "scaled:scale") != 0]
+  
   # intialize w.mat for storing w.vec for each penalty
-  w.mat <- matrix(0, ncol(X.scaled.mat)+1, length(penalty.vec))
+  w.mat <- matrix(0, ncol(X.filtered)+1, length(penalty.vec))
   
   # determine if y.vec is binary and create y.tilde if so
   is.01<- all.y.vec %in% c(0,1)
   
   # initialize opt.thresh and initial.weight.vec to prime loop
   opt.thresh <- 0.01
-  initial.weight.vec <- rep( 0, l=ncol(X.scaled.mat)+1 )
+  initial.weight.vec <- rep( 0, l=ncol(X.filtered)+1 )
   
   # loop through penalty.vec calling LinearModelL1 for each one
   for( penalty in penalty.vec ) {
-    w.tilde.opt.vec <- LinearModelL1( X.scaled.mat, y.vec, penalty, opt.thresh, initial.weight.vec, step.size )
+    w.tilde.opt.vec <- LinearModelL1( X.filtered, y.vec, penalty, opt.thresh, initial.weight.vec, step.size )
     
     # prime initial.weight.vec for warm restart
     initial.weight.vec <- w.tilde.opt.vec
     
     # unscale w.opt.sc.vec and 
-    w.orig <- w.tilde.opt.vec/attr(X.scaled.mat, "scaled:scale")
-    b.orig <- -t(w.tilde.opt.vec/attr(X.scaled.mat, "scaled:scale")) %*% attr(X.scaled.mat, "scaled:center")
+    w.orig <- w.tilde.opt.vec/attr(X.filtered, "scaled:scale")
+    b.orig <- -t(w.tilde.opt.vec/attr(X.filtered, "scaled:scale")) %*% attr(X.filtered, "scaled:center")
     
     # store unscaled result
     w.mat[ , penalty.index ] <- c(b.orig, w.orig)
@@ -170,6 +201,8 @@ LinearModelL1penalties <- function( X.mat, y.vec, penalty.vec, step.size ){
 #' @param step.size int scalar > 1
 #' 
 #' @return mean.validation.loss, mean.train.loss.vec, penalty.vec, selected.penalty, weight.vec, predict=function(testX.mat)
+#' 
+#' @export
 #'
 #' @examples
 #' 
